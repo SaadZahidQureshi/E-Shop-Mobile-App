0712ce856009f09d20e8b3dfb7acbb075a1a594e
Saad Zahid Qureshi <80338270+SaadZahidQureshi@users.noreply.github.com>
2022-06-25 23:56:00 +0500

    theme & fonts
    theme and font style setup

--- app/src/main/java/com/example/e_shop/SplashActivity.kt
+++ app/src/main/java/com/example/e_shop/SplashActivity.kt
@@ -1,11 +1,41 @@
 package com.example.e_shop
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBar
+import androidx.core.view.ViewCompat
 
 class SplashActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        getSupportActionBar()?.hide() // hide action and ttile bar
+        //following code use for full screen view of splash screen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        //following code put delay of 2.5 sec before main activity render
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        },2500
+        )
+
+
     }
 }
---
+++ app/src/main/java/com/example/e_shop/utils/MSPTextViewBold.kt
@@ -0,0 +1,20 @@
+package com.example.e_shop.utils
+
+
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+
+class MSPTextViewBold(context: Context, attrs: AttributeSet) : AppCompatTextView(context,attrs) {
+    init {
+        appFont()
+    }
+
+    private fun appFont() {
+        val typeface : Typeface = Typeface.createFromAsset(context.assets,"Montserrat-Bold.ttf")
+        setTypeface(typeface)
+    }
+
+}
